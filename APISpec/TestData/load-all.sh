490dc47f48b1a17adf82d3b067303792a37f8ff9
Nicholas Rempel <nbrempel@gmail.com>
2018-02-21 13:33:10 -0800

    Update load-all script and README
    Signed-off-by: Nicholas Rempel <nbrempel@gmail.com>

--- APISpec/TestData/load-all.sh
+++ APISpec/TestData/load-all.sh
@@ -1,4 +1,4 @@
-#$/bin/bash
+#!/bin/bash
 export MSYS_NO_PATHCONV=1
 
 # ==============================================================================
@@ -7,11 +7,10 @@ export MSYS_NO_PATHCONV=1
 # * Requires curl
 # ------------------------------------------------------------------------------
 # Usage on Windows (using Git Bash):
-#  ./load-all.sh <server URL>
+#  ./load-all.sh <environment>
 #
 # Example:
 #  ./load-all.sh dev
-#  ./load-all.sh http://localhost:60381
 # ------------------------------------------------------------------------------
 exitOnError () {
   rtnCd=$?
@@ -20,23 +19,13 @@ exitOnError () {
     exit ${rtnCd}
   fi
 }
-# ------------------------------------------------------------------------------
-API_PREFIX=api/v1
-BULK_PATH=bulk
-# ==============================================================================
 
 if [ -z "${1}" ]; then
   echo Incorrect syntax
-  echo USAGE ${0} "<server URL>"
-  echo Example: ${0} dev
-  echo "Where <server URL> is one of dev, test, prod or a full URL"
+  echo USAGE "${0}" "<environment>"
+  echo Example: "${0}" dev
+  echo "Where <environment> is one of local, dev, test, prod or a full URL"
   exit
-fi
-
-# Before starting, remove the cookie authentication file.
-# The ./load.sh script will add it if needed for the remainder of the calls
-if [ -e cookie ]; then
-  rm cookie
 fi
 
 # ==============================================================================================
@@ -45,21 +34,4 @@ fi
 
 echo Data for TheOrgBook is now loaded via the loading of claims. Details to come...
 
-# Users, Roles, and Permissions ...
-# ./load.sh ./${users} ${API_PREFIX}/users/${BULK_PATH} "$@"; exitOnError
-# ./load.sh ./${roles} ${API_PREFIX}/roles/${BULK_PATH} "$@"; exitOnError
-# ./load.sh ./${permissions} ${API_PREFIX}/permissions/${BULK_PATH} "$@"; exitOnError
-# ./load.sh ./${userRole} ${API_PREFIX}/userroles/${BULK_PATH} "$@"; exitOnError
-# ./load.sh ./${rolepermission} ${API_PREFIX}/rolepermissions/${BULK_PATH} "$@"; exitOnError
-
-# Everything else ...
-# ./load.sh ./${InactiveClaimReason} ${API_PREFIX}/inactiveclaimreasons/${BULK_PATH} "$@"; exitOnError
-# ./load.sh ./${Jurisdiction} ${API_PREFIX}/jurisdictions/${BULK_PATH} "$@"; exitOnError
-# ./load.sh ./${VerifiableOrgType} ${API_PREFIX}/verifiableorgtypes/${BULK_PATH} "$@"; exitOnError
-# ./load.sh ./${LocationType} ${API_PREFIX}/locationtypes/${BULK_PATH} "$@"; exitOnError
-# ./load.sh ./${IssuerService} ${API_PREFIX}/issuerservices/${BULK_PATH} "$@"; exitOnError
-# ./load.sh ./${VerifiableClaimType} ${API_PREFIX}/verifiableclaimtypes/${BULK_PATH} "$@"; exitOnError
-# ./load.sh ./${VerifiableOrg} ${API_PREFIX}/verifiableorgs/${BULK_PATH} "$@"; exitOnError
-# ./load.sh ./${VerifiableClaim} ${API_PREFIX}/verifiableclaims/${BULK_PATH} "$@"; exitOnError
-# ./load.sh ./${DoingBusinessAs} ${API_PREFIX}/doingbusinessas/${BULK_PATH} "$@"; exitOnError
-# ./load.sh ./${Location} ${API_PREFIX}/locations/${BULK_PATH} "$@"; exitOnError
+./loadClaims.py "${1}"
