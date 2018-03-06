9f1a70d23dd6148fed8031e35fc38dd80fd20912
Nicholas Rempel <nbrempel@gmail.com>
2018-03-06 14:54:27 -0800

    Successfully storing a claim
    Signed-off-by: Nicholas Rempel <nbrempel@gmail.com>

--- tob-api/api/indy/agent.py
+++ tob-api/api/indy/agent.py
@@ -1,6 +1,7 @@
 import os
 
 from von_agent.nodepool import NodePool
+from von_agent.wallet import Wallet
 from tob_api import hyperledger_indy
 from von_agent.agents import Issuer as VonIssuer
 from von_agent.agents import Verifier as VonVerifier
@@ -25,12 +26,12 @@ class Issuer:
 
         self.instance = VonIssuer(
             self.pool,
-            WALLET_SEED,
-            'TheOrgBook Issuer Wallet',
-            issuer_config,
-            '127.0.0.1',
-            9703,
-            'api/v0')
+            Wallet(
+                self.pool.name,
+                WALLET_SEED,
+                'TheOrgBook Issuer Wallet'
+            )
+        )
 
     async def __aenter__(self):
         await self.pool.open()
@@ -55,12 +56,12 @@ class Verifier:
 
         self.instance = VonVerifier(
             self.pool,
-            WALLET_SEED,
-            'TheOrgBook Verifier Wallet',
-            verifier_config,
-            '127.0.0.1',
-            9703,
-            'api/v0')
+            Wallet(
+                self.pool.name,
+                WALLET_SEED,
+                'TheOrgBook Verifier Wallet'
+            )
+        )
 
     async def __aenter__(self):
         await self.pool.open()
@@ -85,12 +86,12 @@ class Holder:
 
         self.instance = VonHolderProver(
             self.pool,
-            WALLET_SEED,
-            'TheOrgBook Holder Wallet',
-            holder_config,
-            '127.0.0.1',
-            9703,
-            'api/v0')
+            Wallet(
+                self.pool.name,
+                WALLET_SEED,
+                'TheOrgBook Holder Wallet'
+            )
+        )
 
     async def __aenter__(self):
         await self.pool.open()
