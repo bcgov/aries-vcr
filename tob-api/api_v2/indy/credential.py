db233bd86fe302ac4fbd3653e8eb079556f25e71
Nicholas Rempel <nbrempel@gmail.com>
2018-08-20 16:38:46 -0700

    optimize recursive topic credential association query further
    Signed-off-by: Nicholas Rempel <nbrempel@gmail.com>

--- tob-api/api_v2/indy/credential.py
+++ tob-api/api_v2/indy/credential.py
@@ -458,7 +458,11 @@ class CredentialManager(object):
         known_topic_ids = []
 
         def associate_related_topics(topic):
+            if topic in known_topic_ids:
+                return
+            
             credential.topics.add(topic)
+            known_topic_ids.append(topic.id)
             for related_topic in (
                 Topic.objects.filter(credentials__in=topic.credentials.all())
                 .exclude(
@@ -467,7 +471,6 @@ class CredentialManager(object):
                 )
                 .distinct()
             ):
-                known_topic_ids.append(related_topic.id)
                 associate_related_topics(related_topic)
 
         if parent_topic is not None:
