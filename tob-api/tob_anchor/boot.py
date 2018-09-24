fbb5b7dbdbb2bf2da2ee03733eba276184473818
Ian Costanzo <ian@anon-solutions.ca>
2018-09-24 15:21:18 -0700

    bcgov/TheOrgBook#520 TOB verifier wallet seed
    Signed-off-by: Ian Costanzo <ian@anon-solutions.ca>

--- tob-api/tob_anchor/boot.py
+++ tob-api/tob_anchor/boot.py
@@ -148,6 +148,90 @@ def pre_init(proc=False):
         raise
 
 
+    wallet_encryp_key = os.environ.get('WALLET_ENCRYPTION_KEY') or "key"
+
+    ret = {"type": wallet_type}
+
+    if wallet_type == 'postgres':
+        LOGGER.info("Using Postgres storage ...")
+
+        # postgresql wallet-db configuration
+        wallet_host = os.environ.get('POSTGRESQL_WALLET_HOST')
+        if not wallet_host:
+            raise ValueError('POSTGRESQL_WALLET_HOST must be set.')
+        wallet_port = os.environ.get('POSTGRESQL_WALLET_PORT')
+        if not wallet_port:
+            raise ValueError('POSTGRESQL_WALLET_PORT must be set.')
+        wallet_user = os.environ.get('POSTGRESQL_WALLET_USER')
+        if not wallet_user:
+            raise ValueError('POSTGRESQL_WALLET_USER must be set.')
+        wallet_password = os.environ.get('POSTGRESQL_WALLET_PASSWORD')
+        if not wallet_password:
+            raise ValueError('POSTGRESQL_WALLET_PASSWORD must be set.')
+        wallet_admin_user = 'postgres'
+        wallet_admin_password = os.environ.get('POSTGRESQL_WALLET_ADMIN_PASSWORD')
+
+        # TODO pass in as env parameter - key for encrypting the wallet contents
+
+        ret["params"] = {
+            "storage_config": {"url": "{}:{}".format(wallet_host, wallet_port)},
+        }
+        stg_creds = {"account": wallet_user, "password": wallet_password}
+        if wallet_admin_password:
+            stg_creds["admin_account"] = wallet_admin_user
+            stg_creds["admin_password"] = wallet_admin_password
+        ret["access_creds"] = {
+            "key": wallet_encryp_key,
+            "storage_credentials": stg_creds,
+            "key_derivation_method": "ARGON2I_MOD",
+        }
+
+    elif wallet_type == 'sqlite':
+        LOGGER.info("Using Sqlite storage ...")
+        ret["access_creds"] = {"key": wallet_encryp_key}
+    else:
+        raise ValueError('Unknown WALLET_TYPE: {}'.format(wallet_type))
+
+    return ret
+
+def indy_holder_wallet_config(wallet_cfg: dict):
+    wallet_seed = os.environ.get('INDY_WALLET_SEED')
+    if not wallet_seed or len(wallet_seed) is not 32:
+        raise ValueError('INDY_WALLET_SEED must be set and be 32 characters long.')
+
+    if wallet_cfg['type'] == 'postgres':
+        return {
+            "name": "tob_holder",
+            "seed": wallet_seed,
+            "type": "postgres",
+            "params": wallet_cfg["params"],
+            "access_creds": wallet_cfg["access_creds"],
+        }
+    return {
+        "name": "TheOrgBook_Holder_Wallet",
+        "seed": wallet_seed,
+        "access_creds": wallet_cfg["access_creds"],
+    }
+
+def indy_verifier_wallet_config(wallet_cfg: dict):
+    verifier_seed = os.environ.get('INDY_VER_WALLET_SEED') # "TestBCGovSeedforSLNTOBV000000000"
+    if not verifier_seed or len(verifier_seed) is not 32:
+        raise ValueError('INDY_VER_WALLET_SEED must be set and be 32 characters long.')
+
+    if wallet_cfg['type'] == 'postgres':
+        return {
+            "name": "tob_verifier",
+            "seed": verifier_seed,
+            "type": "postgres",
+            "params": wallet_cfg["params"],
+            "access_creds": wallet_cfg["access_creds"],
+        }
+    return {
+        "name": "TheOrgBook_Verifier_Wallet",
+        "seed": verifier_seed,
+        "access_creds": wallet_cfg["access_creds"],
+    }
+
 async def register_services():
 
     await asyncio.sleep(2) # temp fix for messages being sent before exchange has started
