5cc275b7828db3ad464fc14bb0b7f1c42cdb904d
Olena Mitovska <mitovskaol@ON34C02760025.local>
2018-10-05 10:51:54 -0400

    Added tabIndex to all page elements to enable tab nagivation as part of AODA compliance fixes
    Signed-off-by: Olena Mitovska <mitovskaol@ON34C02760025.local>

--- prototypes/tob-web/src/app/general-data.service.ts
+++ prototypes/tob-web/src/app/general-data.service.ts
@@ -29,6 +29,7 @@ export class GeneralDataService {
       return path;
     }
     let root = (<any>window).testApiUrl || this.apiUrl;
+
     if(root) {
       if(! root.endsWith('/')) root += '/';
       return root + path;
