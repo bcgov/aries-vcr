dcb5966139468d89c90bf60f72b79b4334615ea9
Nicholas Rempel <nbrempel@gmail.com>
2019-01-28 10:56:51 -0800

    Add some padding startup banner
    Signed-off-by: Nicholas Rempel <nbrempel@gmail.com>

--- agent/indy_catalyst_agent/logging/__init__.py
+++ agent/indy_catalyst_agent/logging/__init__.py
@@ -17,8 +17,7 @@ class LoggingConfigurator:
         fileConfig(config_path, disable_existing_loggers=False)
 
     @classmethod
-    def print_banner(cls, inbound_transports, outbound_transports):
-        banner_length = 40
+    def print_banner(cls, inbound_transports, outbound_transports, banner_length=40):
 
         banner_title_string = "Indy Catalyst Agent"
         banner_title_spacer = " " * (banner_length - len(banner_title_string))
@@ -34,7 +33,7 @@ class LoggingConfigurator:
         inbound_transport_strings = []
         for transport in inbound_transports:
             host_port_string = (
-                f"• {transport.scheme}://{transport.host}:{transport.port}"
+                f"  • {transport.scheme}://{transport.host}:{transport.port}"
             )
             host_port_spacer = " " * (banner_length - len(host_port_string))
             inbound_transport_strings.append((host_port_string, host_port_spacer))
@@ -47,7 +46,7 @@ class LoggingConfigurator:
         outbound_transport_strings = []
         for schemes in outbound_transports:
             for scheme in schemes:
-                schema_string = f"• {scheme}"
+                schema_string = f"  • {scheme}"
                 scheme_spacer = " " * (banner_length - len(schema_string))
                 outbound_transport_strings.append((schema_string, scheme_spacer))
 
