90ef06d97b642cf97568e2f57f752c51a3108b6a
Ian Costanzo <ian@anon-solutions.ca>
2019-02-14 12:15:55 -0800

    Migration of code from TheOrgBook
    Signed-off-by: Ian Costanzo <ian@anon-solutions.ca>

--- openshift/settings.bc-tob.sh
+++
@@ -1,4 +0,0 @@
-# Setting for the TheOrgBook-BC environments.
-# Uses the existing `devex-von-tools` environment for builds and images
-export TOOLS="devex-von-tools"
-export PROJECT_NAMESPACE="devex-von-bc-tob"
