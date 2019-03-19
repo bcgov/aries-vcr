7b72b1f6ae3e27563e21a11530f16db47c312b06
Nicholas Rempel <nbrempel@gmail.com>
2019-03-19 16:09:18 -0700

    add credential exchange model

--- agent/indy_catalyst_agent/messaging/credentials/manager.py
+++ agent/indy_catalyst_agent/messaging/credentials/manager.py
@@ -2,7 +2,6 @@
 
 import logging
 
-from ..connections.manager import ConnectionManager
 from ..request_context import RequestContext
 from ...error import BaseError
 
@@ -24,23 +23,12 @@ class CredentialManager:
         self._context = context
         self._logger = logging.getLogger(__name__)
 
-    async def create_credential_offer(self, credential_definition_id):
-        await indy.anoncreds.issuer_create_credential_offer(self.wallet.handle, cd_id)
-
-    async def send_credential_offer(
-        self, context, connection_record, schema_name, schema_version
-    ):
-        """
-        Send a credential offer to a connection.
-
-        Args:
-            context: The request context
-            connection_record: The connection to send the credential offer to
-            schema_name: The schema name to send a credential offer for
-            schema_version: The schema version to send a credential offer for
-
-        """
-
-        connection_manager = ConnectionManager(context)
+    async def create_offer(self, credential_definition_id):
 
         pass
+
+
+    async def receive_offer(self, credential_offer):
+
+        pass
+
