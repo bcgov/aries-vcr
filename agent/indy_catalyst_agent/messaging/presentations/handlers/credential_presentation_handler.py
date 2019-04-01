03d03604966197ba4f6899734c2fb4773cdbf53c
Nicholas Rempel <nbrempel@gmail.com>
2019-04-01 12:04:19 -0700

    set up credential presentation model and related boilerplate

---
+++ agent/indy_catalyst_agent/messaging/presentations/handlers/credential_presentation_handler.py
@@ -0,0 +1,27 @@
+"""Basic message handler."""
+
+from ...base_handler import BaseHandler, BaseResponder, RequestContext
+
+from ..manager import PresentationManager
+from ..messages.credential_presentation import CredentialPresentation
+
+
+class CredentialPresentationHandler(BaseHandler):
+    """Message handler class for credential presentations."""
+
+    async def handle(self, context: RequestContext, responder: BaseResponder):
+        """
+        Message handler logic for credential presentations.
+
+        Args:
+            context: request context
+            responder: responder callback
+        """
+        self._logger.debug(f"CredentialPresentationHandler called with context {context}")
+        assert isinstance(context.message, CredentialPresentation)
+        self._logger.info(f"Received credential presentation: {context.message.}")
+
+        credential = context.message.credential_json
+        credential_manager = CredentialManager(context)
+
+        await credential_manager.store_credential(credential)
