02e917e49b79da17af94d18e7fdf98a785c81919
Ian Costanzo <ian@anon-solutions.ca>
2019-04-01 16:42:05 -0700

    Add authentication to hook registration w.i.p.
    Signed-off-by: Ian Costanzo <ian@anon-solutions.ca>

--- credential-registry/server/django-icat-api/api_v2/serializers/hooks.py
+++ credential-registry/server/django-icat-api/api_v2/serializers/hooks.py
@@ -26,27 +26,7 @@ def get_random_password():
 
 def get_password_expiry():
     now = datetime.utcnow().replace(tzinfo=pytz.utc)
-    return now + +timedelta(days=90)
-
-class NewRegistrationSerializer(serializers.Serializer):
-    org_name = serializers.CharField(required=True, max_length=240)
-    email = serializers.CharField(required=True, max_length=128)
-    target_url = serializers.CharField(required=False, max_length=240)
-    hook_token = serializers.CharField(required=False, max_length=240)
-
-    def create(self, validated_data):
-        """
-        Create and return a new instance, given the validated data.
-        """
-        if 'username' in validated_data and 0 < len(validated_data['username']):
-            prefix = validated_data['username'][:16] + "-"
-        else:
-            prefix = "hook-"
-        username = generate_random_username(
-                length=32, prefix=prefix, split=None
-            )
-        return User.objects.create(**validated_data)
-
+    return now + timedelta(days=90)
 
 class RegistrationSerializer(serializers.Serializer):
     org_name = serializers.CharField(required=True, max_length=240)
@@ -54,8 +34,8 @@ class RegistrationSerializer(serializers.Serializer):
     target_url = serializers.CharField(required=False, max_length=240)
     hook_token = serializers.CharField(required=False, max_length=240)
     username = serializers.CharField(required=False, max_length=40)
-    password = serializers.CharField(required=False, max_length=40)
-    registration_expiry = serializers.DateTimeField(required=False)
+    password = serializers.CharField(required=True, max_length=40, write_only=True)
+    registration_expiry = serializers.DateTimeField(required=False, read_only=True)
 
     def create(self, validated_data):
         """
@@ -73,14 +53,19 @@ class RegistrationSerializer(serializers.Serializer):
         # TODO must populate unique DID due to database constraints
         validated_data['DID'] = "not:a:did:" + self.username
 
-        # generate password
-        validated_data['password'] = get_random_password()
+        # TODO generate password (?) for now user must supply
+        #tmp_password = get_random_password()
+        #validated_data['password'] = tmp_password
         validated_data['registration_expiry'] = get_password_expiry()
 
-        user = get_user_model().objects.create(**validated_data)
+        print("Create user with", validated_data['username'], validated_data['password'])
+
+        user = get_user_model().objects.create_user(**validated_data)
 
         user.groups.add(get_subscribers_group())
         user.save()
+
+        #validated_data['gen_password'] = tmp_password
 
         return user
 
@@ -95,14 +80,17 @@ class RegistrationSerializer(serializers.Serializer):
         instance.hook_token = validated_data.get('hook_token', instance.hook_token)
 
         # TODO potentially update password on each update?
-        instance['password'] = get_random_password()
-        validated_data['registration_expiry'] = get_password_expiry()
+        #instance['password'] = get_random_password()
+        if 'password' in validated_data:
+            instance.set_password(validated_data.get('password'))
+            validated_data['registration_expiry'] = get_password_expiry()
 
         instance.save()
         return instance
 
 
 class SubscriptionSerializer(serializers.Serializer):
+    owner = serializers.ReadOnlyField(source='owner.username')
     owner = serializers.CharField(required=True, max_length=40)
     subscription_type = serializers.CharField(required=True, max_length=20)
     topic_source_id = serializers.CharField(required=False, max_length=240)
@@ -114,7 +102,7 @@ class SubscriptionSerializer(serializers.Serializer):
         """
         Create and return a new instance, given the validated data.
         """
-        owner = User.objects.filter(username=validated_data['owner']).first()
+        owner = request.user
         validated_data['owner'] = owner
         credential_type = CredentialType.objects.filter(schema__name=validated_data['credential_type']).first()
         validated_data['credential_type'] = credential_type
--- credential-registry/server/django-icat-api/api_v2/views/hooks.py
+++ credential-registry/server/django-icat-api/api_v2/views/hooks.py
@@ -2,6 +2,8 @@ import logging
 from rest_framework import status
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
+from rest_framework.permissions import *
+from rest_framework.exceptions import *
 from django.http import JsonResponse
 from django.contrib.auth import get_user_model
 from rest_framework.viewsets import ReadOnlyModelViewSet, ViewSet
@@ -23,20 +25,82 @@ from api_v2.models.Subscription import Subscription
 from api_v2.serializers.hooks import *
 
 
+class IsOwnerOrCreateOnly(permissions.BasePermission):
+    """
+    Permission check for subscription ownership.
+    """
+    def has_permission(self, request, view):
+        print("IsOwnerOrCreateOnly view permission check ...")
+        if request.user.is_authenticated:
+            print("request.user", request.user)
+        else:
+            print("request.user is not authenticated")
+        ret = super().has_permission(request, view)
+        print(" >>> returns ", ret)
+        return ret
+
+    def has_object_permission(self, request, view, obj):
+        print("IsOwnerOrCreateOnly.has_object_permission()")
+        if isinstance(obj, get_user_model()):
+            print("user", request.user)
+            if request.user.is_authenticated:
+                print("obj", obj)
+                return obj == request.user
+            elif request.method == 'POST':
+                # creating a new user is ok
+                return True
+        print("IsOwnerOrCreateOnly obj permission check returns False")
+        return False
+
+class IsOwnerOnly(permissions.BasePermission):
+    """
+    Permission check for subscription ownership.
+    """
+    def has_permission(self, request, view):
+        print("IsOwnerOnly view permission check ...")
+        if request.user.is_authenticated:
+            print("request.user", request.user)
+        else:
+            print("request.user is not authenticated")
+        ret = super().has_permission(request, view)
+        print(" >>> returns ", ret)
+        return ret
+        
+    def has_object_permission(self, request, view, obj):
+        print("IsOwnerOnly.has_object_permission()")
+        if isinstance(obj, Subscription):
+            print("user", request.user)
+            if request.user.is_authenticated:
+                print("obj.owner", obj.owner)
+                return obj.owner == request.user
+        print("IsOwnerOnly obj permission check returns False")
+        return False
+
 class RegistrationViewSet(ModelViewSet):
     """
     This viewset automatically provides `list`, `create`, `retrieve`,
     `update` and `destroy` actions.
     """
-    #queryset = User.objects.all()
     serializer_class = RegistrationSerializer
     lookup_field = 'username'
-    #permission_classes = (permissions.IsAuthenticatedOrReadOnly,
-    #                      IsOwnerOrReadOnly,)
-    permission_classes = ()
+    permission_classes = (IsOwnerOrCreateOnly,)
 
     def get_queryset(self):
-        return get_user_model().objects.filter(groups__name=SUBSCRIBERS_GROUP_NAME).all()
+        if self.request.user.is_authenticated:
+            return get_user_model().objects.filter(groups__name=SUBSCRIBERS_GROUP_NAME, username=self.request.user.username).all()
+        else:
+            raise NotAuthenticated()
+
+    def get_object(self):
+        if self.request.user.is_authenticated:
+            if self.request.user.username == self.kwargs["username"]:
+                obj = get_object_or_404(self.get_queryset(), username=self.kwargs["username"])
+                self.check_object_permissions(self.request, obj)
+                return obj
+            else:
+                raise PermissionDenied()
+        else:
+            raise NotAuthenticated()
 
     def perform_create(self, serializer):
         serializer.save()
@@ -46,11 +110,8 @@ class SubscriptionViewSet(ModelViewSet):
     This viewset automatically provides `list`, `create`, `retrieve`,
     `update` and `destroy` actions.
     """
-    #queryset = Subscription.objects.filter(owner__username=self.kwargs['username']).all()
     serializer_class = SubscriptionSerializer
-    #permission_classes = (permissions.IsAuthenticatedOrReadOnly,
-    #                      IsOwnerOrReadOnly,)
-    permission_classes = ()
+    permission_classes = (IsAuthenticated, IsOwnerOnly,)
 
     def get_queryset(self):
         return Subscription.objects.filter(owner__username=self.kwargs['registration_username']).all()
