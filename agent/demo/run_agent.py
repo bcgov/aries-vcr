25f3b038c5b92aadb2bae11c6016e2f8e9c5fc44
Ian Costanzo <ian@anon-solutions.ca>
2019-05-09 08:11:00 -0700

    Agent demo w.i.p.
    Signed-off-by: Ian Costanzo <ian@anon-solutions.ca>

--- agent/demo/run_agent.py
+++ agent/demo/run_agent.py
@@ -9,12 +9,14 @@ import random
 
 def output_reader(proc):
     for line in iter(proc.stdout.readline, b''):
-        print('got line: {0}'.format(line.decode('utf-8')), end='')
+        #print('got line: {0}'.format(line.decode('utf-8')), end='')
+        pass
 
 
 def stderr_reader(proc):
     for line in iter(proc.stderr.readline, b''):
-        print('got line: {0}'.format(line.decode('utf-8')), end='')
+        #print('got line: {0}'.format(line.decode('utf-8')), end='')
+        pass
 
 
 def main():
@@ -30,19 +32,41 @@ PYTHONPATH=.. ../scripts/icatagent \
     my_env = os.environ.copy()
     my_env["PYTHONPATH"] = ".."
 
+    # TODO figure out how to start and expose a REST callback service
+    my_env["WEBHOOK_URL"] = "TBD"
+
+    # TODO genesis transactions from file or url
     with open('local-genesis.txt', 'r') as genesis_file:
         genesis = genesis_file.read()
-    print(genesis)
+    #print(genesis)
 
+    # TODO seed from input parameter; optionally register the DID
+    rand_name = str(random.randint(100000, 999999))
+    seed = ('my_seed_000000000000000000000000' + rand_name)[-32:]
+    alias = 'My Test Company'
+    register_did = True
+    if register_did:
+        print("Registering", alias, "with seed", seed)
+        ledger_url = 'http://localhost:9000'
+        headers = {"accept": "application/json"}
+        data = {"alias": alias, "seed": seed, "role": "TRUST_ANCHOR"}
+        resp = requests.post(ledger_url+'/register', json=data)
+        nym_info = resp.text
+        print(nym_info)
+
+    # start agent sub-process
     proc = subprocess.Popen(['python3', '../scripts/icatagent', 
                             '--inbound-transport', 'http', '0.0.0.0', '8000', 
                             '--inbound-transport', 'http', '0.0.0.0', '8001', 
                             '--inbound-transport', 'ws', '0.0.0.0', '8002',
                             '--outbound-transport', 'ws', 
                             '--outbound-transport', 'http', 
+                            '--auto-respond-messages',
                             '--genesis-transactions', genesis,
                             '--wallet-type', 'indy',
-                            '--seed', 'my_seed_000000000000000000000666',
+                            '--wallet-name', 'faber'+rand_name,
+                            '--wallet-key', 'faber'+rand_name,
+                            '--seed', seed,
                             '--admin', '0.0.0.0', '8080'],
                             stdout=subprocess.PIPE,
                             stderr=subprocess.PIPE,
@@ -57,16 +81,9 @@ PYTHONPATH=.. ../scripts/icatagent \
         time.sleep(0.2)
 
         # check swagger content
-        resp = urllib.request.urlopen('http://localhost:8080/api/docs/swagger.json')
-        p = resp.read()
-        assert b'Indy Catalyst Agent' in p
-
-        # get an invitation
-        headers = {"accept": "application/json"}
-        req = urllib.request.Request('http://localhost:8080/connections/create-invitation', headers=headers, method='POST')
-        resp = urllib.request.urlopen(req)
-        p = resp.read()
-        print(p)
+        resp = requests.get('http://localhost:8080/api/docs/swagger.json')
+        p = resp.text
+        assert 'Indy Catalyst Agent' in p
 
         # create a schema
         version = format("%d.%d.%d" % (random.randint(1, 101), random.randint(1, 101), random.randint(1, 101)))
@@ -81,6 +98,7 @@ PYTHONPATH=.. ../scripts/icatagent \
         schema_id = schema_response_body["schema_id"]
         print(schema_id)
 
+        # create a cred def for the schema
         credential_definition_body = {"schema_id": schema_id}
         credential_definition_response = requests.post(
             f"http://localhost:8080/credential-definitions", json=credential_definition_body
@@ -91,6 +109,14 @@ PYTHONPATH=.. ../scripts/icatagent \
         ]
 
         print(f"cred def id: {credential_definition_id}")
+
+        # generate an invitation
+        headers = {"accept": "application/json"}
+        resp = requests.post('http://localhost:8080/connections/create-invitation', headers=headers)
+        p = resp.text
+        print("*****************")
+        print("Invitation:", p)
+        print("*****************")
 
     except Exception as e:
         print(e)
