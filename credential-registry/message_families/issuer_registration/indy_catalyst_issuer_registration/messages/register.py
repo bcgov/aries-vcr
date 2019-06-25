5da18dd2c176c0cac849ce73e6bb87bdd91a7cfe
Nicholas Rempel <nbrempel@gmail.com>
2019-06-25 16:12:13 -0700

    nested should be provided instances not classes
    Signed-off-by: Nicholas Rempel <nbrempel@gmail.com>

--- credential-registry/message_families/issuer_registration/indy_catalyst_issuer_registration/messages/register.py
+++ credential-registry/message_families/issuer_registration/indy_catalyst_issuer_registration/messages/register.py
@@ -70,7 +70,7 @@ class IssuerRegistrationSchema(AgentMessageSchema):
             class Credential(Schema):
                 """Nested credential schema."""
 
-                effective_date = fields.Nested(CredentialMapping, required=True)
+                effective_date = fields.Nested(CredentialMapping(), required=True)
 
             class MappingEntry(Schema):
                 """Nested mapping entry schema."""
@@ -84,44 +84,44 @@ class IssuerRegistrationSchema(AgentMessageSchema):
                     _type = fields.Nested(
                         CredentialMapping, data_key="type", required=False
                     )
-                    value = fields.Nested(CredentialMapping, required=False)
+                    value = fields.Nested(CredentialMapping(), required=False)
 
-                _fields = fields.Nested(Fields, data_key="fields", required=True)
+                _fields = fields.Nested(Fields(), data_key="fields", required=True)
                 model = fields.Str(required=True)
 
             class Topic(Schema):
                 """Nested topic schema."""
 
-                source_id = fields.Nested(CredentialMapping, required=False)
+                source_id = fields.Nested(CredentialMapping(), required=False)
                 _type = fields.Nested(
-                    CredentialMapping, data_key="type", required=False
+                    CredentialMapping(), data_key="type", required=False
                 )
                 name = fields.Nested(CredentialMapping, required=False)
-                related_source_id = fields.Nested(CredentialMapping, required=False)
-                related_type = fields.Nested(CredentialMapping, required=False)
-                related_name = fields.Nested(CredentialMapping, required=False)
+                related_source_id = fields.Nested(CredentialMapping(), required=False)
+                related_type = fields.Nested(CredentialMapping(), required=False)
+                related_name = fields.Nested(CredentialMapping(), required=False)
 
             cardinality_fields = fields.Dict(required=False)
             caregory_labels = fields.Dict(required=False)
             claim_descriptions = fields.Dict(required=False)
             claim_labels = fields.Dict(required=False)
 
-            credential = fields.Nested(Credential, required=False)
+            credential = fields.Nested(Credential(), required=False)
 
             name = fields.Str(required=True)
             schema = fields.Str(required=True)
             version = fields.Str(required=True)
             description = fields.Str(required=False)
 
-            mapping = fields.List(fields.Nested(MappingEntry), required=False)
-            topic = fields.List(fields.Nested(Topic), required=True)
+            mapping = fields.List(fields.Nested(MappingEntry()), required=False)
+            topic = fields.List(fields.Nested(Topic()), required=True)
 
             logo_b64 = fields.Str(required=False)
             credential_def_id = fields.Str(required=True)
             endpoint = fields.Str(required=False)
             visible_fields = fields.List(fields.Str, required=False)
 
-        issuer = fields.Nested(IssuerSchema, required=True)
-        credential_types = fields.List(fields.Nested(CredentialType), required=False)
+        issuer = fields.Nested(IssuerSchema(), required=True)
+        credential_types = fields.List(fields.Nested(CredentialType()), required=False)
 
-    issuer_registration = fields.Nested(IssuerRegistrationNestedSchema, required=True)
+    issuer_registration = fields.Nested(IssuerRegistrationNestedSchema(), required=True)
