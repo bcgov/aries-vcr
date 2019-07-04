28f355d78faecbcc2b59b91207376a146afdb511
Ian Costanzo <ian@anon-solutions.ca>
2019-07-04 10:32:40 -0700

    Fixed nested dicts in issuer registration message
    Signed-off-by: Ian Costanzo <ian@anon-solutions.ca>

--- credential-registry/message_families/issuer_registration/indy_catalyst_issuer_registration/routes.py
+++ credential-registry/message_families/issuer_registration/indy_catalyst_issuer_registration/routes.py
@@ -81,8 +81,8 @@ class IssuerRegistrationRequestSchema(Schema):
             cardinality_fields = fields.Dict(required=False)
             category_labels = fields.Dict(required=False)
 
-            claim_descriptions = fields.Dict(keys=fields.Str(required=False), values=fields.Dict(required=False), required=False)
-            claim_labels = fields.Dict(keys=fields.Str(required=False), values=fields.Dict(required=False), required=False)
+            claim_descriptions = fields.Dict(keys=fields.Str(), values=fields.Dict(), required=False)
+            claim_labels = fields.Dict(keys=fields.Str(), values=fields.Dict(), required=False)
 
             credential = fields.Nested(Credential(), required=False)
 
