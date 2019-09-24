2b07913e0e65790dd880a42587332110075041bc
Emiliano Suñé <emiliano.sune@gmail.com>
2019-09-24 12:33:30 -0700

    Applied formatter to routing.py
    Signed-off-by: Emiliano Suñé <emiliano.sune@gmail.com>

--- starter-kits/credential-registry/server/tob-api/api_v2/middleware/routing.py
+++ starter-kits/credential-registry/server/tob-api/api_v2/middleware/routing.py
@@ -11,7 +11,7 @@ class HTTPHeaderRoutingMiddleware(object):
     else if there is a version requested in the URL it will use it,
     else it will use the version identified as "default".
 
-    The Accept header (if supplied) will be rewritted as a regular content header (as mapped in settings) and the 
+    The Accept header (if supplied) will be rewritten as a regular content header (as mapped in settings) and the 
     version written into the URL will be as mapped in the settings.
 
     The actual API URL's must be mapped under "/api/v2/...", "/api/v3/..." etc. in the Django urlconf files.
@@ -38,27 +38,49 @@ class HTTPHeaderRoutingMiddleware(object):
 
     def process_request(self, request):
         # only process for API calls
-        if request.path_info.startswith(settings.HTTP_HEADER_ROUTING_MIDDLEWARE_URL_FILTER + "/"):
+        if request.path_info.startswith(
+            settings.HTTP_HEADER_ROUTING_MIDDLEWARE_URL_FILTER + "/"
+        ):
             # check the ACCEPT header for version override
             version = None
-            if 'HTTP_ACCEPT' in request.META:
+            if "HTTP_ACCEPT" in request.META:
                 for content_type in settings.HTTP_HEADER_ROUTING_MIDDLEWARE_ACCEPT_MAP:
-                    if (request.META['HTTP_ACCEPT'].find(content_type) != -1):
+                    if request.META["HTTP_ACCEPT"].find(content_type) != -1:
                         # replace the ACCEPT header with a standard resource format
-                        accept_header = request.META['HTTP_ACCEPT']
-                        request.META['HTTP_ACCEPT'] = settings.HTTP_HEADER_ROUTING_MIDDLEWARE_ACCEPT_MAP[content_type]
+                        accept_header = request.META["HTTP_ACCEPT"]
+                        request.META[
+                            "HTTP_ACCEPT"
+                        ] = settings.HTTP_HEADER_ROUTING_MIDDLEWARE_ACCEPT_MAP[
+                            content_type
+                        ]
 
                         # parse out version number (if available)
                         idx = accept_header.find("version=")
                         if 0 <= idx:
-                            version = accept_header[idx+len("version="):].lower()
+                            version = accept_header[idx + len("version=")].lower()
 
             # if there is a version requested in the path, use it
             request_path_info = request.path_info
             for allowed_version in settings.HTTP_HEADER_ROUTING_MIDDLEWARE_VERSION_MAP:
-                if request_path_info.startswith(settings.HTTP_HEADER_ROUTING_MIDDLEWARE_URL_FILTER + "/" + allowed_version + "/"):
+                if request_path_info.startswith(
+                    settings.HTTP_HEADER_ROUTING_MIDDLEWARE_URL_FILTER
+                    + "/"
+                    + allowed_version
+                    + "/"
+                ):
                     # remove version info from the path (we will inject it later)
-                    request_path_info = settings.HTTP_HEADER_ROUTING_MIDDLEWARE_URL_FILTER + "/" + request_path_info[len(settings.HTTP_HEADER_ROUTING_MIDDLEWARE_URL_FILTER + "/" + allowed_version + "/"):]
+                    request_path_info = (
+                        settings.HTTP_HEADER_ROUTING_MIDDLEWARE_URL_FILTER
+                        + "/"
+                        + request_path_info[
+                            len(
+                                settings.HTTP_HEADER_ROUTING_MIDDLEWARE_URL_FILTER
+                                + "/"
+                                + allowed_version
+                                + "/"
+                            )
+                        ]
+                    )
                     if not version:
                         version = allowed_version
 
@@ -70,13 +92,21 @@ class HTTPHeaderRoutingMiddleware(object):
             use_version = None
             for allowed_version in settings.HTTP_HEADER_ROUTING_MIDDLEWARE_VERSION_MAP:
                 if version == allowed_version:
-                    use_version = settings.HTTP_HEADER_ROUTING_MIDDLEWARE_VERSION_MAP[version]
+                    use_version = settings.HTTP_HEADER_ROUTING_MIDDLEWARE_VERSION_MAP[
+                        version
+                    ]
             if not use_version:
                 return
-            
-            # rewrite the requested version into the path
-            request.path_info = settings.HTTP_HEADER_ROUTING_MIDDLEWARE_URL_FILTER + "/" + use_version + request_path_info[len(settings.HTTP_HEADER_ROUTING_MIDDLEWARE_URL_FILTER:]
 
+            # rewrite the requested version into the path
+            request.path_info = (
+                settings.HTTP_HEADER_ROUTING_MIDDLEWARE_URL_FILTER
+                + "/"
+                + use_version
+                + request_path_info[
+                    len(settings.HTTP_HEADER_ROUTING_MIDDLEWARE_URL_FILTER)
+                ]
+            )
 
     def process_response(self, request, response):
         return response
