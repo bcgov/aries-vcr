5d2369eeb2a18d54d1e6b49ee00fea8f289a2689
Nicholas Rempel <nbrempel@gmail.com>
2019-12-06 16:56:59 -0800

    Add gevent workers  to celery workers
    Signed-off-by: Nicholas Rempel <nbrempel@gmail.com>

--- starter-kits/credential-registry/server/tob-api/start-celery-worker.sh
+++ starter-kits/credential-registry/server/tob-api/start-celery-worker.sh
@@ -2,4 +2,4 @@
 
 # Start the tob-api as a Celery worker node.
 echo "Starting an instance of the tob-api as a Celery worker node ..."
-celery -A icat_hooks worker -E -l INFO
+celery -A icat_hooks worker -P gevent -c 10 -E -l INFO
