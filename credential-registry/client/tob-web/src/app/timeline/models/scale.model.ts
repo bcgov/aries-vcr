898d48063c23f8e822d5c7d267b880ac3c0efe9d
seanadipose <hamiltonsean6@icloud.com>
2019-12-20 20:44:45 +0000

    changed the scale for short lived credentials from 10 years to 1. Corrected sizing issue for long credentials
    Signed-off-by: seanadipose <hamiltonsean6@icloud.com>

--- credential-registry/client/tob-web/src/app/timeline/models/scale.model.ts
+++ credential-registry/client/tob-web/src/app/timeline/models/scale.model.ts
@@ -1,14 +1,6 @@
-export type scaleOpts = 1 | 2 | 3 | 4
+export type scaleOpts = 1 | 2 | 3 | 4;
 
-export class Scale {
-
-  constructor() {
-
-  }
-}
-// This is stubbed to be a full class depending on how complex we need to be
 export function timelineScale(start: Date, end: Date) {
-
-  const scale =  end.getFullYear() - start.getFullYear()
-  return scale < 10 ? 1 : 2;
+  const scale = end.getFullYear() - start.getFullYear();
+  return scale < 2 ? 1 : 2;
 }
--- credential-registry/client/tob-web/src/app/timeline/services/timeline.service.ts
+++ credential-registry/client/tob-web/src/app/timeline/services/timeline.service.ts
@@ -3,35 +3,33 @@ import { RowSpec, MarkerSpec } from 'app/timeline/models/i-timeline';
 import { parseDate } from 'app/timeline/models/timeline-utils';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class TimelineService {
-
   genShortDateMarkers(rows: RowSpec[]): MarkerSpec[] {
-    const markers = []
+    const markers = [];
     for (const row of rows) {
-      row.slots.forEach((itm) => {
+      row.slots.forEach(itm => {
         if (itm.end == null) return;
-        const diff = parseDate(itm.end).getTime() - parseDate(itm.start).getTime()
+        const diff = parseDate(itm.end).getTime() - parseDate(itm.start).getTime();
         const monthCount = diff / 2592000000;
-        if (monthCount > 1 && monthCount < 12) {
-
+        if (monthCount > 0 && monthCount < 12) {
           markers.push({
             date: parseDate(itm.start),
             label: this.genLabel(itm.htmlContent.toString()),
             link: itm.url,
-        })
-      }
-    })
-    return markers;
+          });
+        }
+      });
     }
+    return markers;
   }
 
   genLabel(html: string) {
-    let label = html.slice(html.indexOf('>') + 1, html.indexOf('</strong>'));
-    if (label.length > 20) label = label.slice(0, 20) + '...'
-    return label
+    let label = html.slice(html.indexOf('</span>') + 7, html.indexOf('</strong>'));
+    if (label.length > 20) label = label.slice(0, 20);
+    return label;
   }
 
-  constructor() { }
+  constructor() {}
 }
