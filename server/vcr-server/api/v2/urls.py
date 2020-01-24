ed4d311356824c147e42dc90c0e5d2900cd25150
Nicholas Rempel <nbrempel@gmail.com>
2020-01-23 16:04:25 -0800

    Move swagger out of v2 module, add v3 boilerplate
    Signed-off-by: Nicholas Rempel <nbrempel@gmail.com>

--- server/vcr-server/api/v2/urls.py
+++ server/vcr-server/api/v2/urls.py
@@ -1,29 +1,9 @@
-from django.conf import settings
 from django.urls import path
-from drf_yasg import openapi
-from drf_yasg.views import get_schema_view
-from rest_framework.permissions import AllowAny
 from rest_framework.routers import SimpleRouter
 from rest_framework.urlpatterns import format_suffix_patterns
 
 from api.v2.views import misc, rest, search
 from api.v2.utils import get_stats, clear_stats
-
-API_METADATA = settings.API_METADATA
-schema_view = get_schema_view(
-    openapi.Info(
-        title=API_METADATA["title"],
-        default_version="v2",
-        description=API_METADATA["description"],
-        terms_of_service=API_METADATA["terms"]["url"],
-        contact=openapi.Contact(**API_METADATA["contact"]),
-        license=openapi.License(**API_METADATA["license"]),
-    ),
-    # url="{}/api".format(settings.APPLICATION_URL),
-    validators=["flex", "ssv"],
-    public=True,
-    permission_classes=(AllowAny,),
-)
 
 router = SimpleRouter(trailing_slash=False)
 
@@ -53,8 +33,4 @@ miscPatterns = [
     path("status", get_stats),
 ]
 
-swaggerPatterns = [
-    path("", schema_view.with_ui("swagger", cache_timeout=None), name="api-docs")
-]
-
-urlpatterns = format_suffix_patterns(router.urls) + miscPatterns + swaggerPatterns
+urlpatterns = format_suffix_patterns(router.urls) + miscPatterns
--- server/vcr-server/vcr_server/urls.py
+++ server/vcr-server/vcr_server/urls.py
@@ -2,6 +2,12 @@
 Definition of urls for app.
 """
 
+from drf_yasg import openapi
+from drf_yasg.views import get_schema_view
+
+from rest_framework.permissions import AllowAny
+
+from django.conf import settings
 from django.urls import include, path
 from django.views.generic import RedirectView
 
@@ -10,6 +16,23 @@ from . import views
 # Uncomment the next two lines to enable the admin:
 # from django.contrib import admin
 # admin.autodiscover()
+
+API_METADATA = settings.API_METADATA
+schema_view = get_schema_view(
+    openapi.Info(
+        title=API_METADATA["title"],
+        description=API_METADATA["description"],
+        default_version="v3",
+        terms_of_service=API_METADATA["terms"]["url"],
+        contact=openapi.Contact(**API_METADATA["contact"]),
+        license=openapi.License(**API_METADATA["license"]),
+    ),
+    # url="{}/api".format(settings.APPLICATION_URL),
+    validators=["flex", "ssv"],
+    public=True,
+    permission_classes=(AllowAny,),
+)
+
 
 base_patterns = [
     path("api-auth/", include("rest_framework.urls", namespace="rest_framework")),
@@ -24,7 +47,9 @@ hook_patterns = [
 api_patterns = [
     path("", RedirectView.as_view(url="api/"), name="api-root"),
     path("api", RedirectView.as_view(url="api/"), name="api"),
+    path("docs/", schema_view.with_ui("swagger", cache_timeout=None), name="api-docs"),
     path("api/v2/", include("api.v2.urls"), name="api-v2"),
+    path("api/v3/", include("api.v3.urls"), name="api-v3"),
 ]
 
 urlpatterns = base_patterns + hook_patterns + api_patterns
