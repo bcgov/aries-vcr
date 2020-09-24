4ae02027f814019a38977fce2d1f16fd1e2439a7
Ian Costanzo <ian@anon-solutions.ca>
2020-09-24 08:29:37 -0700

    Tinker with message types
    Signed-off-by: Ian Costanzo <ian@anon-solutions.ca>

--- server/message_families/issuer_registration/indy_catalyst_issuer_registration/message_types.py
+++ server/message_families/issuer_registration/indy_catalyst_issuer_registration/message_types.py
@@ -1,7 +1,7 @@
 """Message type identifiers for Issuer Registrations."""
 
 DID_PREFIX = "did:sov:BzCbsNYhMrjHiqZDTUASHg;spec";
-MESSAGE_FAMILY = "/issuer-registration/1.0"
+MESSAGE_FAMILY = "issuer-registration/1.0"
 
 MESSAGE_TYPE = f"{MESSAGE_FAMILY}/register"
 
