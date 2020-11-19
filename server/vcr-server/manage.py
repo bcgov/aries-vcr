27e4d449ddcbd72c60a8f498a73a597f11db3a15
Emiliano Suñé <emiliano.sune@gmail.com>
2020-11-19 12:45:36 -0800

    Enable remote debugging for vcr-server
    Signed-off-by: Emiliano Suñé <emiliano.sune@gmail.com>

--- server/vcr-server/manage.py
+++ server/vcr-server/manage.py
@@ -6,9 +6,25 @@ Command-line utility for administrative tasks.
 import os
 import sys
 
-if __name__ == "__main__":
+
+def main():
     os.environ.setdefault("DJANGO_SETTINGS_MODULE", "vcr_server.settings")
+
+    from django.conf import settings
+
+    if settings.DEBUG:
+        if os.environ.get("RUN_MAIN") or os.environ.get("WERKZEUG_RUN_MAIN"):
+            import debugpy
+
+            debugpy.listen(("0.0.0.0", 3000))
+            print("Remote debugger listening on port 3000")
+            debugpy.wait_for_client()  # blocks execution until client is attached
+            print("Remote debugger attached")
 
     from django.core.management import execute_from_command_line
 
     execute_from_command_line(sys.argv)
+
+
+if __name__ == "__main__":
+    main()
