bab0a12419a9c13844abedc7fbb985e3e5fcd97b
Akiff Manji <akiff.manji@gmail.com>
2020-12-24 19:04:40 -0800

    Fix search query in basic search
    Signed-off-by: Akiff Manji <akiff.manji@gmail.com>

--- client/src/app/search/form.component.ts
+++ client/src/app/search/form.component.ts
@@ -9,7 +9,7 @@ import { Subscription } from 'rxjs';
 
 const FilterSpec = [
   {
-    name: "name",
+    name: "q",
     alias: "query",
     hidden: true
   },
@@ -131,7 +131,7 @@ export class SearchComponent implements OnInit, OnDestroy, AfterViewInit {
 
   ngAfterViewInit() {
     if(this._searchInput) {
-      this._searchInput.value = this._filters.getFieldValue('name');
+      this._searchInput.value = this._filters.getFieldValue('q');
     }
   }
 
@@ -198,7 +198,7 @@ export class SearchComponent implements OnInit, OnDestroy, AfterViewInit {
       this._loader.reset();
       this._refresh = true;
       this._filters.update({
-        name: this._searchInput.value,
+        q: this._searchInput.value,
         page: '1'
       });
     }
@@ -206,18 +206,18 @@ export class SearchComponent implements OnInit, OnDestroy, AfterViewInit {
 
   public initSearch() {
     if(this._searchInput)
-      this._searchInput.value = this._filters.getFieldValue('name');
+      this._searchInput.value = this._filters.getFieldValue('q');
     this._inited = true;
     this._performSearch();
   }
 
   get blankQuery() {
-    return this._filters.getFieldValue('name') === '';
+    return this._filters.getFieldValue('q') === '';
   }
 
   _performSearch() {
     let query = this._filters.values;
-    if('name' in query && query['name'] !== '') {
+    if('q' in query && query['q'] !== '') {
       this._dataService.loadList(this._loader, {query});
     } else {
       this._loader.reset();
