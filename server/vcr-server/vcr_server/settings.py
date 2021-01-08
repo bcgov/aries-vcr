9f2d7098bb3ae99e948677514ab7952554774a41
Akiff Manji <akiff.manji@gmail.com>
2021-01-08 09:34:40 -0800

    Restores DEBUG flags to resolve swagger page loading
    Signed-off-by: Akiff Manji <akiff.manji@gmail.com>

--- server/vcr-server/vcr_server/settings.py
+++ server/vcr-server/vcr_server/settings.py
@@ -47,7 +47,8 @@ SECRET_KEY = os.getenv(
 )
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = parse_bool(os.getenv("DJANGO_DEBUG", "False"))
+DEBUG = parse_bool(os.getenv("DEBUG", "True"))
+DJANGO_DEBUG = parse_bool(os.getenv("DJANGO_DEBUG", "False"))
 
 DEMO_SITE = parse_bool(os.getenv("DEMO_SITE", "False"))
 
