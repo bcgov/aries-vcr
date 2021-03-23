2c49f00bb4a8d3db7ac92fc2b8ff88a91ac98d1d
Akiff Manji <akiff.manji@gmail.com>
2021-03-23 13:24:25 -0700

    Prevent duplicate autocomplete results from displaying
    Signed-off-by: Akiff Manji <akiff.manji@gmail.com>

--- client/src/app/general-data.service.ts
+++ client/src/app/general-data.service.ts
@@ -142,15 +142,19 @@ export class GeneralDataService {
     return this._recordCounts[type] || 0;
   }
 
-  autocomplete(term, inactive: '' | boolean = false): Observable<Object> {
+  autocomplete(term, inactive: '' | boolean = false, revoked: '' | boolean = false): Observable<Object> {
     console.log('inactive', inactive);
     if (term === '' || typeof term !== 'string') {
       return from([]);
     }
-    let params = new HttpParams().set('q', term).append('inactive', inactive.toString());
+
+    let params = new HttpParams().set('q', term)
+      .append('inactive', inactive.toString())
+      .append('revoked', revoked.toString());
+
     return this.loadFromApi('v3/search/autocomplete', params)
       .pipe(
-        map(({ results }: { results? }) => results || []),
+        map(({ results }: { results?}) => results || []),
         map(results => results.map(row => ({
           id: row.id,
           term: row.value
