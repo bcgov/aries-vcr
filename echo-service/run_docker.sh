c37e120ed3f714c03806e7419c4f2114efdb6f75
Ian Costanzo <ian@anon-solutions.ca>
2021-04-01 10:32:28 -0700

    Fix echo app docker script
    Signed-off-by: Ian Costanzo <ian@anon-solutions.ca>

--- echo-service/run_docker.sh
+++ echo-service/run_docker.sh
@@ -1,2 +1,2 @@
-docker build echo-app -f ../docker/echo-service/Dockerfile .
+docker build -t echo-app -f ../docker/echo-service/Dockerfile .
 docker run -p 8000:8000 -it --rm --name echo-app echo-app sh start.sh
