c04a27fda64b1f9d362d8d013203fa1140de41db
Akiff Manji <akiff.manji@gmail.com>
2021-05-26 13:45:29 -0700

    Adds fuzzy search endpoint to directly query solr
    Signed-off-by: Akiff Manji <akiff.manji@gmail.com>

---
+++ server/vcr-server/api/v4/views/search/fuzzy.py
@@ -0,0 +1,45 @@
+import pysolr
+import json
+
+from rest_framework import permissions, status
+from rest_framework.response import Response
+from rest_framework.viewsets import ViewSet
+
+from drf_yasg import openapi
+from drf_yasg.utils import swagger_auto_schema
+
+from vcr_server.haystack import config
+
+from api.v4.serializers.search.fuzzy import SearchSerializer
+
+# Create a solr client instance.
+solr_client = pysolr.Solr(config()['URL'], always_commit=True)
+
+swagger_params = [
+    # Put additional parameters here
+    openapi.Parameter(
+        "q",
+        openapi.IN_QUERY,
+        description="Enter an Apache Leucene query that will return matching Solr indexes",
+        type=openapi.TYPE_STRING,
+    )
+]
+
+
+class SearchView(ViewSet):
+    """
+    ViewSet that will forward an Apache Leucene query directly to Solr
+    """
+    permission_classes = (permissions.AllowAny,)
+
+    @swagger_auto_schema(manual_parameters=swagger_params)
+    def list(self, request):
+        try:
+            query_params = request.query_params
+            q = query_params.get('q', None)
+            if (not q):
+                return Response("Query parameter \'q\' is required", status.HTTP_400_BAD_REQUEST)
+            results = solr_client.search(f'topic_name_suggest:{q}')
+            return Response(SearchSerializer(results, many=True).data)
+        except Exception as e:
+            return Response(f'There was a problem with the request', status.HTTP_500_INTERNAL_SERVER_ERROR)
